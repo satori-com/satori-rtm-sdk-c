a611c1dbb2b6ad68b2b362668757ea96bf10b9b3
Andrey Vasenin <avasenin@satori.com>
2017-03-29 17:22:03 +0200

    Update code samples

--- core/samples/auth_publish_subscribe.c
+++ core/samples/auth_publish_subscribe.c
@@ -3,40 +3,39 @@
 #include <string.h>
 #include <time.h>
 
-#ifdef _WIN32
-#include <Windows.h>
-static void sleep(int seconds) {
-    return Sleep(seconds);
-}
-#else
-#include <unistd.h>
-#endif
-
 #include <rtm.h>
+
+#define NONCE_SIZE 32
 
 static char const *endpoint = "wss://myapp.api.satori.com/";
 static char const *appkey = "my_appkey";
-static char const *channel = "my_channel";
-
 static char const *role = "my_role";
 static char const *role_secret = "my_secret";
 
-static char *last_nonce = NULL;
-static int authenticated = 0;
+static char const *channel = "my_channel";
+
+typedef struct {
+  int authenticated;
+  char nonce[NONCE_SIZE];
+} client_state;
 
 void my_pdu_handler(rtm_client_t *rtm, const rtm_pdu_t *pdu) {
+  client_state *state = (client_state *) rtm_get_user_context(rtm);
   switch (pdu->action) {
     case RTM_ACTION_HANDSHAKE_OK:
-      printf("Copying nonce %s\n", pdu->nonce);
-      last_nonce = (char *)malloc(strlen(pdu->nonce) + 1);
-      strcpy(last_nonce, pdu->nonce);
+      strncpy(state->nonce, pdu->nonce, NONCE_SIZE - 1);
       break;
     case RTM_ACTION_AUTHENTICATE_OK:
-      authenticated = 1;
+      fprintf(stderr, "Authentication succeed\n");
+      state->authenticated = 1;
       break;
+    case RTM_ACTION_GENERAL_ERROR:
+    case RTM_ACTION_PUBLISH_ERROR:
+    case RTM_ACTION_HANDSHAKE_ERROR:
+    case RTM_ACTION_SUBSCRIPTION_ERROR:
+    case RTM_ACTION_SUBSCRIBE_ERROR:
     case RTM_ACTION_AUTHENTICATE_ERROR:
-      fprintf(stderr, "Authentication error: %s\n", pdu->error);
-      fprintf(stderr, "Authentication error reason: %s\n", pdu->reason);
+      fprintf(stderr, " Error: %s - %s\n", pdu->error, pdu->reason);
       break;
     case RTM_ACTION_SUBSCRIPTION_DATA: {
       printf("Subscription data\n");
@@ -54,35 +53,32 @@ void my_pdu_handler(rtm_client_t *rtm, const rtm_pdu_t *pdu) {
 int authenticate(rtm_client_t *rtm) {
   unsigned request_id;
   rtm_status rc = rtm_handshake(rtm, role, &request_id);
-  if (rc) {
+  client_state *state = (client_state *) rtm_get_user_context(rtm);
+
+  if (RTM_OK != rc) {
     fprintf(stderr, "Failed to send handshake request\n");
     return rc;
   }
 
   rc = rtm_wait_timeout(rtm, 10 /* seconds */);
-  if (rc) {
+  if (RTM_OK != rc) {
     fprintf(stderr, "Failed to receive handshake reply\n");
     return rc;
   }
 
-  if (!last_nonce) {
-    fprintf(stderr, "Failed to get nonce from the handshake reply\n");
-    return RTM_ERR_PARAM;
-  }
-
-  rc = rtm_authenticate(rtm, role_secret, last_nonce, &request_id);
-  if (rc) {
+  rc = rtm_authenticate(rtm, role_secret, state->nonce, &request_id);
+  if (RTM_OK != rc) {
     fprintf(stderr, "Failed to send authenticate request\n");
     return rc;
   }
 
   rc = rtm_wait_timeout(rtm, 10 /* seconds */);
-  if (rc) {
+  if (RTM_OK != rc) {
     fprintf(stderr, "Failed to receive authenticate reply\n");
     return rc;
   }
 
-  if (!authenticated) {
+  if (!state->authenticated) {
     fprintf(stderr, "Authentication failed\n");
     return RTM_ERR_PARAM;
   }
@@ -92,35 +88,32 @@ int authenticate(rtm_client_t *rtm) {
 
 int main(int argc, const char *argv[]) {
   rtm_client_t *rtm = (rtm_client_t *)malloc(rtm_client_size);
-  unsigned request_id;
+  client_state state = {0};
   rtm_status rc;
 
-  rc = rtm_connect(rtm, endpoint, appkey, &my_pdu_handler, NULL);
+  rc = rtm_connect(rtm, endpoint, appkey, &my_pdu_handler, &state);
 
-  if (rc) {
+  if (RTM_OK != rc) {
     fprintf(stderr, "Unable to connect to RTM: %s\n", rtm_error_string(rc));
-    free(rtm);
-    return rc;
+    goto cleanup;
   }
 
   fprintf(stderr, "Connected to RTM\n");
 
   rc = authenticate(rtm);
-  if (rc) {
-      rtm_close(rtm);
-      free(rtm);
-      return rc;
+  if (RTM_OK != rc) {
+    goto cleanup;
   }
 
   fprintf(stderr, "Authenticated as %s\n", role);
 
+  unsigned request_id;
   rc = rtm_subscribe(rtm, channel, &request_id);
-  if (rc) {
+  if (RTM_OK != rc) {
     fprintf(stderr, "Unable to subscribe to channel: %s\n", rtm_error_string(rc));
-    rtm_close(rtm);
-    free(rtm);
-    return rc;
+    goto cleanup;
   }
+
   rtm_publish_string(rtm, channel, "Hello world!", NULL);
   for (int i = 0; i < 100; i++) {
     char buffer[1024];
@@ -129,22 +122,23 @@ int main(int argc, const char *argv[]) {
   }
 
   fprintf(stderr, "Subscribed\n");
-  sleep(1);
 
   time_t start_time = time(NULL);
   while (1) {
     rc = rtm_wait_timeout(rtm, 10);
-    if (rc != RTM_OK)
+    if (RTM_OK != rc)
       break;
     // loop for a total of 10s.
     if (time(NULL) - start_time > 10)
       break;
   }
 
-  rtm_close(rtm);
 
   fprintf(stderr, "Done\n");
+  goto cleanup;
 
-  free(rtm);
-  return rc;
+  cleanup:
+    rtm_close(rtm);
+    free(rtm);
+    return rc;
 }
