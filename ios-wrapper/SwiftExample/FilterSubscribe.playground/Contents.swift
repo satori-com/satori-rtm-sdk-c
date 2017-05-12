2c493b0449ca5bcf8590e7ca76e3f0e856f25901
Abhishek Angne <aangne@satori.com>
2017-05-12 13:04:21 -0700

    Added Rich documentation to the swift playgrounds

--- ios-wrapper/SwiftExample/FilterSubscribe.playground/Contents.swift
+++ ios-wrapper/SwiftExample/FilterSubscribe.playground/Contents.swift
@@ -2,12 +2,16 @@ import MapKit
 import PlaygroundSupport
 import SatoriRtmSdkWrapper
 //: ![Satori](SatoriLogo.jpg)
-//: ## Using Filters with Open Data Channels
-
-let url : String = "wss://open-data.api.satori.com"
+//: ## Using Filters with Open Data Channel
+/*:
+ [Meetup-RSVP Channel]: https://www.satori.com/channels/Meetup-RSVP
+ - Important:
+ Replace AppKeyForMeetupRSVP with the Appkey for [Meetup-RSVP Channel]
+ */
 let appKey = "AppKeyForMeetupRSVP"
+let url : String = "wss://open-data.api.satori.com"
 let channelName = "Meetup-RSVP"
-
+//: __MapAnnotation class for placing Annotations on the map__
 class MapAnnotation: NSObject, MKAnnotation {
     var mapCoordinate: CLLocationCoordinate2D;
     
@@ -19,16 +23,13 @@ class MapAnnotation: NSObject, MKAnnotation {
         return mapCoordinate
     }
 }
-
-// set the zoom
+//: __Set the zoom level and size for map__
 let delta = 1.0
-
-// set the size of the map
 let frame = CGRect( x:0, y:0, width:600, height:600 )
 let mapView = MKMapView( frame: frame )
-// view the map in the timeline!
+//: __View the map in the timeline__
 PlaygroundPage.current.liveView = mapView
-
+//: __Define the PduHandler. This will be called by Satori rtm when there is activity for the subscribe success/error response and subscription data responses.__
 let handler : PduHandler = {(SatoriPdu) -> Void in
     let action : String = SatoriPdu.action;
     if action == "rtm/subscription/data" {
@@ -47,21 +48,23 @@ let handler : PduHandler = {(SatoriPdu) -> Void in
         }
     }
 }
-
+//: __Connect to Satori using SatoriRtmConnection. And subscribe to Meetup-RSVP channel using a View(formerly Filter) to show locations in the US where meetups are happening in real time__
+/*:
+ - Note:
+ It is recommended to connect to Satori on a background queue as network activity on the main thread is not performance efficient. You may also create your own operation queues and dispatch the connection logic to those queues.
+ */
 DispatchQueue.global(qos: .background).async {
     let conn : SatoriRtmConnection? = SatoriRtmConnection(url: url, andAppkey: appKey);
     conn?.connect(pduHandler: handler)
     var requestId:UInt32 = 123;
-    let filter = "select count(*) as '#of participants', group.group_city as city, group.group_country as country, group.group_lon as longitude, group.group_lat as latitude from `Meetup-RSVP` where country like \"us\" group by city";
+    let view = "select count(*) as '#of participants', group.group_city as city, group.group_country as country, group.group_lon as longitude, group.group_lat as latitude from `Meetup-RSVP` where country like \"us\" group by city";
     
-    let body : NSDictionary = NSDictionary(objects: [filter,channelName], forKeys: ["filter" as NSCopying,"subscription_id" as NSCopying]);
+    let body : NSDictionary = NSDictionary(objects: [view,channelName], forKeys: ["filter" as NSCopying,"subscription_id" as NSCopying]);
     
     do {
         let jsonData = try JSONSerialization.data(withJSONObject: body, options: .prettyPrinted)
         // here "jsonData" is the dictionary encoded in JSON data
-        
         let str : String? = String(data: jsonData, encoding: .utf8);
-        
         conn?.subscribe(withBody: str!, andRequestId: &requestId);
         while ((conn?.poll().rawValue)! >= 0) {
             sleep(1);
@@ -70,5 +73,3 @@ DispatchQueue.global(qos: .background).async {
         print(error.localizedDescription)
     }
 }
-
-
--- ios-wrapper/SwiftExample/SimpleSubscribe.playground/Contents.swift
+++ ios-wrapper/SwiftExample/SimpleSubscribe.playground/Contents.swift
@@ -1,34 +1,38 @@
 import UIKit
 import PlaygroundSupport
 import SatoriRtmSdkWrapper
-
+//: ![Satori](SatoriLogo.jpg)
 //: ## Subscribing to an Open Data Channel
-
-let url : String = "wss://open-data.api.satori.com"
+/*:
+ [big-rss Channel]: https://www.satori.com/channels/big-rss
+ - Important:
+ Replace AppKeyForBigRss with the Appkey for [big-rss Channel]
+ */
 let appKey = "AppKeyForBigRss"
+let url : String = "wss://open-data.api.satori.com"
 let channelName = "big-rss"
-
+//: __Container view to host the textView__
 let hostView = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 800))
 hostView.backgroundColor = .lightGray
 PlaygroundPage.current.liveView = hostView
-
-let titleTextView = UITextView(frame: .zero)
-titleTextView.translatesAutoresizingMaskIntoConstraints = false
-titleTextView.text = ""
-titleTextView.isEditable = false;
-titleTextView.isScrollEnabled = true;
-titleTextView.font = UIFont(name: "Menlo-Regular", size: 13)
-titleTextView.textColor = .white
-titleTextView.backgroundColor = .black
-
-let hostStackView = UIStackView(arrangedSubviews: [titleTextView])
+//: __TextView that shows RSS Title and Published On information__
+let rssTextView = UITextView(frame: .zero)
+rssTextView.translatesAutoresizingMaskIntoConstraints = false
+rssTextView.text = ""
+rssTextView.isEditable = false;
+rssTextView.isScrollEnabled = true;
+rssTextView.font = UIFont(name: "Menlo-Regular", size: 13)
+rssTextView.textColor = .white
+rssTextView.backgroundColor = .black
+//: __Create a StackView to contain the rssTextView and add it to hostView__
+let hostStackView = UIStackView(arrangedSubviews: [rssTextView])
 hostStackView.frame = hostView.bounds
 hostStackView.axis = .vertical
 hostStackView.spacing = 1
-titleTextView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+rssTextView.heightAnchor.constraint(equalToConstant: 200).isActive = true
 hostView.addSubview(hostStackView)
 hostView
-
+//: __Define the PduHandler. This will be called by Satori rtm when there is activity for the subscribe success/error response and subscription data responses.__
 let handler : PduHandler = {(SatoriPdu) -> Void in
     let action : String = SatoriPdu.action;
     if action == "rtm/subscription/data" {
@@ -39,15 +43,19 @@ let handler : PduHandler = {(SatoriPdu) -> Void in
             let title = (msg.object(forKey: "title") as! String);
             let publishedTs = (msg.object(forKey: "publishedTimestamp") as! String);
             DispatchQueue.main.async {
-                titleTextView.text = titleTextView.text + "\n -------------------------------------------------------";
-                titleTextView.text = titleTextView.text + "\n Title:" + title + "\n Published On:" + publishedTs + "\n";
-                let range = NSMakeRange(titleTextView.text.characters.count - 1, 1);
-                titleTextView.scrollRangeToVisible(range);
+                rssTextView.text = rssTextView.text + "\n -------------------------------------------------------";
+                rssTextView.text = rssTextView.text + "\n Title:" + title + "\n Published On:" + publishedTs + "\n";
+                let range = NSMakeRange(rssTextView.text.characters.count - 1, 1);
+                rssTextView.scrollRangeToVisible(range);
             }
         }
     }
 }
-
+//: __Connect to Satori using SatoriRtmConnection. And subscribe to big-rss channel to show title and published on date in real time__
+/*:
+ - Note:
+ It is recommended to connect to Satori on a background queue as network activity on the main thread is not performance efficient. You may also create your own operation queues and dispatch the connection logic to those queues.
+ */
 DispatchQueue.global(qos: .background).async {
     let conn : SatoriRtmConnection? = SatoriRtmConnection(url: url, andAppkey: appKey);
     conn?.connect(pduHandler: handler)
