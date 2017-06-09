2cff5ad5d83ba57d8e6246aefe4e970f219af161
Dmitry Ivanov <divanov@satori.com>
2017-06-09 17:46:12 +0200

    Update ios-wrapper for core changes

--- ios-wrapper/SatoriRtmSdkWrapper/SatoriRtmSdkWrapper/SatoriPdu.h
+++ ios-wrapper/SatoriRtmSdkWrapper/SatoriRtmSdkWrapper/SatoriPdu.h
@@ -1,15 +1,15 @@
 #import <Foundation/Foundation.h>
-#import <SatoriSDK/rtm.h>
+#import <SatoriRtmSdkWrapper/rtm.h>
 
 @interface SatoriPdu : NSObject
 
 @property (nonatomic, readonly) enum rtm_action_t action;
-@property (nonatomic, readonly) NSDictionary *_Nonnull fields;
+@property (nonatomic, readonly) NSDictionary *_Nonnull body;
 @property (nonatomic, readonly) unsigned requestId;
 
-- (nullable instancetype)init __attribute__((unavailable("Must use initWithAction:fields:andRequestId: instead.")));
-- (nullable instancetype)initWithAction:(enum rtm_action_t)action fields:(NSDictionary *_Nonnull)fields andRequestId:(unsigned)requestId;
-- (nullable instancetype)initWithLowLevelPdu:(const rtm_pdu_t *_Nonnull)pdu;
+- (nullable instancetype)init __attribute__((unavailable("Must use initWithAction:body:andRequestId: instead.")));
+- (nullable instancetype)initWithAction:(enum rtm_action_t)action body:(NSDictionary *_Nonnull)body andRequestId:(unsigned)requestId;
+- (nullable instancetype)initWithRawPdu:(char const *_Nonnull)pdu;
 
 
 @end
