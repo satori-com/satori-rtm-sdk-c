8bc18e2aed0e0b9bade875698a9524e126a0e72b
Dmitry Ivanov <divanov@satori.com>
2017-06-19 15:09:30 +0200

    Untangle waiting for publish reply and for subscription data

--- tutorial/main.c
+++ tutorial/main.c
@@ -144,9 +144,7 @@ int main(void) {
     // want to read from an open data channel.
     status = handshake_and_authenticate(rtm);
     if (RTM_OK != status) {
-      rtm_close(rtm);
-      free(rtm);
-      return status;
+      goto cleanup;
     }
 
     printf("Authenticated as %s\n", role);
@@ -157,18 +155,14 @@ int main(void) {
   status = rtm_subscribe(rtm, channel, &request_id);
   if (RTM_OK != status) {
     fprintf(stderr, "Unable to send subscribe request: %s\n", rtm_error_string(status));
-    rtm_close(rtm);
-    free(rtm);
-    return status;
+    goto cleanup;
   }
 
   // Wait for a subscribe reply
   status = rtm_wait_timeout(rtm, 10 /* seconds */);
   if (status != RTM_OK) {
     fprintf(stderr, "Failed to receive subscribe reply: %s\n", rtm_error_string(status));
-    rtm_close(rtm);
-    free(rtm);
-    return status;
+    goto cleanup;
   }
 
   // Note that RTM_OK result above refers only to the fact that we
@@ -178,11 +172,8 @@ int main(void) {
   // which was or was not set to 1 in that process.
   if (!tutorial_state.subscribe_ok) {
     fprintf(stderr, "Subscribe reply was an error\n");
-    rtm_close(rtm);
-    free(rtm);
-    return status;
+    goto cleanup;
   }
-
 
   while (1) {
     tutorial_state.got_message = 0;
@@ -192,52 +183,32 @@ int main(void) {
     status = rtm_publish_json(rtm, channel, message_as_json, &request_id);
     if (status != RTM_OK) {
       fprintf(stderr, "Failed to send publish request: %s\n", rtm_error_string(status));
-      rtm_close(rtm);
-      free(rtm);
-      return status;
+      goto cleanup;
     }
 
-    // Now we're expecting two PDUs to arrive: a publish reply one and (if publish
-    // succeeded) a subscription data one. There is no guarantee of which one comes
-    // first.
-    // Successful status from call to rtm_wait_timeout means that one or more PDUs
-    // were processed. Note that we say "one or more" and it is important because we
-    // cannot just call rtm_wait_timeout exactly twice for the two expected
-    // messagesWe need to check if we got both PDUs after the first call. And repeat
-    // the call if not.
-    // Real world applications are more complex than this tutorial and will likely
-    // have more convenient abstraction for getting PDUs, but that's out of scope of
-    // this tutorial. C Core SDK doesn't provide such built-in abstraction.
-    int i = 0;
-    for (i = 0; i < 2; ++i) {
+    while (!tutorial_state.publish_ok) {
       status = rtm_wait_timeout(rtm, 10 /* seconds */);
       if (status != RTM_OK) {
-        fprintf(stderr, "Failed to receive publish reply or subscription data: %s\n", rtm_error_string(status));
-        rtm_close(rtm);
-        free(rtm);
-        return status;
+        fprintf(stderr, "Failed to receive publish reply: %s\n", rtm_error_string(status));
+        goto cleanup;
       }
+    }
 
-      if (tutorial_state.publish_ok && tutorial_state.got_message) {
-        break;
+    while (!tutorial_state.got_message) {
+      status = rtm_wait_timeout(rtm, 10 /* seconds */);
+      if (status != RTM_OK) {
+        fprintf(stderr, "Failed to receive subscription data: %s\n", rtm_error_string(status));
+        goto cleanup;
       }
-    }
-    if (!tutorial_state.publish_ok) {
-      fprintf(stderr, "Publish reply was an error\n");
-      rtm_close(rtm);
-      free(rtm);
-      return status;
-    }
-    if (!tutorial_state.got_message) {
-      fprintf(stderr, "Didn't receive the message\n");
-      rtm_close(rtm);
-      free(rtm);
-      return status;
     }
 
     fflush(stdout);
-    fflush(stderr);
 
     rtm_wait_timeout(rtm, 2);
   }
+
+cleanup:
+  rtm_close(rtm);
+  free(rtm);
+  return status;
 }
