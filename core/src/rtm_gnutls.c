561d4257b414331bd73299da7bb9aae5936cd4dc
Phillip Berndt <pberndt@mz.com>
2017-08-28 15:54:45 +0200

    Add missing errno header

--- core/src/rtm_gnutls.c
+++ core/src/rtm_gnutls.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include "rtm_internal.h"
 
--- core/src/rtm_windows.c
+++ core/src/rtm_windows.c
@@ -5,6 +5,7 @@
 #include <io.h>
 #include <string.h>
 #include <stdio.h>
+#include <errno.h>
 
 #include <panzi/portable_endian.h>
 
