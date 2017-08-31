f9341740ea0525ee443e1bba8c9d452a844dd01b
Phillip Berndt <pberndt@mz.com>
2017-08-31 10:24:18 +0200

    Always use _rtm_isspace

--- core/src/rtm_json.c
+++ core/src/rtm_json.c
@@ -5,14 +5,13 @@
 
 #include "rtm_internal.h"
 
-#ifdef RTM_HAS_ISSPACE
-  #include <ctype.h>
-#else
-  static int _rtm_isspace(int c) {
-    return c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ';
-  }
-  #define isspace _rtm_isspace
-#endif
+static int _rtm_isspace(int c) {
+  // Use our own isspace() since ESP8266/Arduino have faulty implementations.
+  // GCC and clang are smart enough to optimise the function away if a native
+  // isspace() is available.
+  return c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ';
+}
+#define isspace _rtm_isspace
 
 static char *find_end_of_element(char *json) {
   int brackets = 0;
